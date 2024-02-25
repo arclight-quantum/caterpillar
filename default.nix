2370c57e207a7fbc11cc8d47a430834c340d2c86
gjz010 <gjz010944@gmail.com>
2024-02-25 22:27:48 +0800

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 {llvmPackages, fetchFromGitHub, cmake, ninja, fmt, nlohmann_json
-,pkgconfig}:
+,pkg-config}:
 let
   stdenv = llvmPackages.stdenv;
 in
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
   pname = "caterpillar";
   version = "0.0.1";
   src = ./.;
-  nativeBuildInputs = [ cmake ninja pkgconfig ];
+  nativeBuildInputs = [ cmake ninja pkg-config ];
   propagatedBuildInputs = [ nlohmann_json fmt ];
   cmakeFlags = [
     "-DCATERPILLAR_TEST=OFF"
